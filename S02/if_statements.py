e95681b54c7d14bd17d565b28b825446c55e7f29
Vahid Naeini <naeini.v@gmail.com>
2018-10-11 04:47:26 -0700

    if statement: elif/else

--- S02/if_statements.py
+++ S02/if_statements.py
@@ -6,3 +6,19 @@ if num <= upper and num >= lower:
     print('the number is in the range')
 else:
     print('the number is out of range')
+
+
+if num % 3 == 0:
+    print('number is n = 3k')
+else:
+    if num % 3 == 1:
+        print('number is n = 3k+1')
+    else:
+        print('number is n = 3k+2')
+
+if num % 3 == 0:
+    print('number is n = 3k')
+elif num % 3 == 1:
+    print('number is n = 3k+1')
+else:
+    print('number is n = 3k+2')
