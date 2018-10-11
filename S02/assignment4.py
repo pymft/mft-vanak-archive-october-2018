1f2caf9fe4df6b3c13cac07ac5a9d57a0e9961e7
Vahid Naeini <naeini.v@gmail.com>
2018-10-11 07:11:52 -0700

    solution to guess numbers problem :smile:

--- S02/assignment4.py
+++ S02/assignment4.py
@@ -1,2 +1,14 @@
 import random
 secret = random.randint(0, 10)
+
+
+for i in range(10):
+    num = input('guess my secret number: ')
+    num = int(num)
+    if num > secret:
+        print('my number is lesser than what said')
+    elif num < secret:
+        print('my number is greater than what said')
+    else:
+        print("BINGO")
+        break
