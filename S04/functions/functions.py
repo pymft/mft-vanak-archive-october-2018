608be1d1bbd9a2d64bd66e419a636f292256e0a5
Vahid Naeini <naeini.v@gmail.com>
2018-10-18 02:52:28 -0700

    review S03

---
+++ S04/functions/functions.py
@@ -0,0 +1,15 @@
+def myfunction(a, b, c):
+    d = a + b + c
+    return d
+
+def fact(n):
+    if n == 0:
+        return 1
+    return n * fact(n-1)
+
+def test_function(a, b):
+    c = a + b
+    return c
+    print("hello")
+
+value = myfunction(10, 20, 1)
