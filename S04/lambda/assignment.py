1705f83a1873d3fb976fc99d7c9a8a21c9ece22f
Vahid Naeini <naeini.v@gmail.com>
2018-10-18 05:41:23 -0700

    lambda assignment and sorting

--- S04/lambda/assignment.py
+++ S04/lambda/assignment.py
@@ -1,6 +1,9 @@
 # f1(x:tuple) -> diff_values of tuple
 # f1((1, 10)) -> 9
-
+f1 = lambda x: x[1] - x[0]
 # f2(s:str) -> reverted string
 # f2('hello') -> 'olleh'
+f2 = lambda s: s[::-1]
 
+res = f2('hello')
+print (res)
---
+++ S04/sorting/default_sort_function.py
@@ -0,0 +1,16 @@
+lst = [1, 50, 32, 90, 30, 0, 80, 45]
+print(lst)
+lst.sort()
+print(lst)
+
+
+print('-'*60)
+lst = [1, 50, 32, 90, 30, 0, 80, 45]
+lst_new = sorted(lst)
+print(lst)
+print(lst_new)
+
+lst_of_tuples = [(1, 10), (20, 30), (20, -1), (31, -1), (50, 4)]
+print(lst_of_tuples)
+lst_of_tuples.sort()
+print(lst_of_tuples)
