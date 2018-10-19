c1f07d17345dcb85c828887c63a101aa58d4071c
Vahid Naeini <naeini.v@gmail.com>
2018-10-19 07:21:05 -0700

    assignment : the wasteland

---
+++ S05/strings quick review/assignment.py
@@ -0,0 +1,8 @@
+from poem import wasteland
+
+# 1. print a list of unique words used in wasteland
+
+
+# 2. print a dictionary of words and their occurrences in text
+#    key would be the word itself and value (an integer) is the number of repeats
+
