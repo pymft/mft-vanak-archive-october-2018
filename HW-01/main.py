c6135bb51f8c08a4967718609ba3005072e20e60
Vahid Naeini <naeini.v@gmail.com>
2018-10-19 16:22:04 +0000

    hw no. 1

---
+++ HW-01/main.py
@@ -0,0 +1,8 @@
+from etefagh import data
+
+
+# 1. duration
+
+# 2. alias values by chars, say, S for short periods, M, L for medium and long periods respectively
+
+# 3. find patterns like LSL, LSSL, LSSSL, LSSSSL
