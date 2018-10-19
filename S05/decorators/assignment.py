72686b57e5fde7c4c16c8a26c581e50419bd4b5e
Vahid Naeini <naeini.v@gmail.com>
2018-10-19 07:14:15 -0700

    continue: decorators

--- S05/decorators/assignment.py
+++ S05/decorators/assignment.py
@@ -1,9 +1,24 @@
 # write decorator named `timeit`
+import time
+
+
+def timeit(f):
+    def inner(*args, **kwargs):
+        t1 = time.time()
+        out = f(*args, **kwargs)
+        t2 = time.time()
+        print("elapsed time = ", t2-t1, "sec")
+        return out
+    return inner
+
 
 @timeit
-def whatever(n):
-    # blaah blaah
-    return
+def zeta(s=2, n=100000):
+    res = 0
+    for i in range(1, n+1):
+        res += 1 / (i ** s)
+    return res
 
 
+print(zeta())
 # elapsed time = 0.123123412312 sec
