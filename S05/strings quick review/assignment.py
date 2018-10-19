3578c2ebde206c4b54027caf42290cfecd6e695d
Vahid Naeini <naeini.v@gmail.com>
2018-10-19 07:24:31 -0700

    wasteland - 2

--- S05/strings quick review/assignment.py
+++ S05/strings quick review/assignment.py
@@ -6,3 +6,7 @@ from poem import wasteland
 # 2. print a dictionary of words and their occurrences in text
 #    key would be the word itself and value (an integer) is the number of repeats
 
+# 3. 10 words most repeated
+
+# 4. top 10 five-or-more-character-length words most repeated
+
