4d564401743e507ae87efcaf88f41159270b9cfb
Vahid Naeini <naeini.v@gmail.com>
2018-10-25 03:25:54 -0700

    Revert "please help me ostad"
    This reverts commit 540adf7

--- S05/strings quick review/assignment.py
+++ S05/strings quick review/assignment.py
@@ -2,18 +2,6 @@ from poem import wasteland
 
 # 1. print a list of unique words used in wasteland
 
-def wasteland(word):
-    wasteland1 = []
-    wasteland1.append(word[0])
-    for i in range (len(word)):
-        for j in range (len(wasteland1)):
-            if word[i]==word[j]:
-                pass
-            else:
-                wasteland1.append(word[i])
-                print(wasteland1[j])
-    print(wasteland1)
-    return wasteland1
 
 # 2. print a dictionary of words and their occurrences in text
 #    key would be the word itself and value (an integer) is the number of repeats
