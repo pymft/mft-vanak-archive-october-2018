f8685e3e78240fa37fd06a18cf97ba971974cc45
Vahid Naeini <naeini.v@gmail.com>
2018-10-25 04:05:59 -0700

    formatting integers

--- S06/review.py
+++ S06/review.py
@@ -1,12 +1,26 @@
 num = 10
 value = 20
-print("number = ", num)
+# print("number = ", num)
+#
+# print("number = "+ str(num))
+# #     |<------------------>|
+# #     |<---------->|
+# print("number = %s" % (num))
+#
+# print("number = %s and value = %s" % (value, range))
+# print("number = " + str(value) + " and value = " + str(range))
 
-print("number = "+ str(num))
-#     |<------------------>|
-#     |<---------->|
-print("number = %s" % (num))
+#  numbers (int) : %d
 
-print("number = %s and value = %s" % (value, range))
-print("number = " + str(value) + " and value = " + str(range))
+print("my integer%d." % num)
+print("my integer%10d." % num)   # right justified
+print("my integer%-10d." % num)   # left justified
+print("my integer%010d." % num)   # right justified
 
+num2 = 10000000000000000
+print("my integer%10d." % num2)   # right justified
+
+
+#  floats
+var = 10.23
+print("my integer%f." % var)
