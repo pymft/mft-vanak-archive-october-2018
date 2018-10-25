ff6f1b9286581916e60182505eec3a609a2301b7
Vahid Naeini <naeini.v@gmail.com>
2018-10-25 04:09:22 -0700

    floats formatting

--- S06/review.py
+++ S06/review.py
@@ -11,16 +11,21 @@ value = 20
 # print("number = " + str(value) + " and value = " + str(range))
 
 #  numbers (int) : %d
-
-print("my integer%d." % num)
-print("my integer%10d." % num)   # right justified
-print("my integer%-10d." % num)   # left justified
-print("my integer%010d." % num)   # right justified
-
-num2 = 10000000000000000
-print("my integer%10d." % num2)   # right justified
-
+#
+# print("my integer%d." % num)
+# print("my integer%10d." % num)   # right justified
+# print("my integer%-10d." % num)   # left justified
+# print("my integer%010d." % num)   # right justified
+#
+# num2 = 10000000000000000
+# print("my integer%10d." % num2)   # right justified
+#
 
 #  floats
-var = 10.23
-print("my integer%f." % var)
+var = 10.27
+print("my integer%fx" % var)        # my integer10.270000x
+print("my integer%20fx" % var)     # my integer           10.270000x
+print("my integer%-20fx" % var)     # my integer10.2700             x
+print("my integer%-20.4fx" % var)   # my integer10.2700             x
+print("my integer%20.1fx" % var)    # my integer                10.3x
+
