ac5521177e7a1746e257a2ed4d2c6a1b1cf17112
Vahid Naeini <naeini.v@gmail.com>
2018-10-25 07:00:49 -0700

    file manipulation

--- S06/file_io/read_from_file.py
+++ S06/file_io/read_from_file.py
@@ -1,13 +1,18 @@
-f = open('input.txt', mode='r', encoding='utf-8')
+f = open('input.txt', mode='r+', encoding='utf-8')
 
-text = f.read(6)
-print(type(text))
-print(text)
-text = f.read()
-print(text)
-# while True:
-#     text = f.read(6)
-#     if not text:
-#         break
-#     print(text)
+# f.seek(3)
+f.seek(11*2 + 4)
+text = f.write('hi')
+
+# print(text)
+# text = f.read(6)
+# print(type(text))
+# print(text)
+# text = f.read()
+# print(text)
+# # while True:
+# #     text = f.read(6)
+# #     if not text:
+# #         break
+# #     print(text)
 f.close()
