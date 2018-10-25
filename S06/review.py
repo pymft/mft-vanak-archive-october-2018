2b8b5a9e853534baf6e5ffa1f4c7a19be7659d12
Vahid Naeini <naeini.v@gmail.com>
2018-10-25 04:00:43 -0700

    formatting with %

---
+++ S06/review.py
@@ -0,0 +1,12 @@
+num = 10
+value = 20
+print("number = ", num)
+
+print("number = "+ str(num))
+#     |<------------------>|
+#     |<---------->|
+print("number = %s" % (num))
+
+print("number = %s and value = %s" % (value, range))
+print("number = " + str(value) + " and value = " + str(range))
+
