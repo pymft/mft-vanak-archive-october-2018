c72f2198a60a5aed80e65632a3eb3629ee14a1bf
Vahid Naeini <naeini.v@gmail.com>
2018-11-02 06:25:12 -0700

    oop - crawler assignment- save method  :smile:

--- S09/assignment_2.py
+++ S09/assignment_2.py
@@ -8,6 +8,7 @@ class Brower:
     def __init__(self, url):
         self.url = url
         self.__text = ''
+        # FIXME : do not allow to instantiate duplicated url
 
     @property
     def text(self):
@@ -16,13 +17,19 @@ class Brower:
             pass
         return self.__text
 
-    def save(self, name):
+    def save(self, name=None):
         """
         :param str name: path to save html file
         :return:
         """
         # use `open` function, mode `w`
-        pass
+
+        # fixme: use with ,
+        if name is None:
+            pass
+            # how to find site's title via urllib.request
+        with open(name, 'w') as f:
+            f.write(self.text)
 
     def get_links(self):
         """
@@ -32,4 +39,5 @@ class Brower:
 
 
 if __name__ == '__main__':
-    b1 = Brower('https://www.google.com')
+    b1 = Brower('https://www.python.org')
+    b1.save('./python.html')
