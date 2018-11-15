d245fc6090be479d5749c55e0e338f7f1c0379b9
Vahid Naeini <naeini.v@gmail.com>
2018-11-15 05:15:53 -0800

    sqlite

--- S10/db/sqlite.py
+++ S10/db/sqlite.py
@@ -3,4 +3,15 @@
 # ----table-----
 # insert, update, select, delete
 
+import sqlite3
 
+
+db_file = 'chinook.db'
+conn = sqlite3.connect(db_file)
+
+res = conn.execute("select * from tracks")
+# for row in res:
+#     print(row)
+
+conn.execute("update person set firstname = 'John' where id = 1")
+conn.commit()
